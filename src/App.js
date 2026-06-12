a1c2de96958b8f74ddb95b3965e9bf17fc9935d0
kororo11 <kororo11@naver.com>
2026-06-12 09:40:37 +0900

    App.js 수정 및 index.html 타이틀 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>안녕하세요! test-branch 입니다. 🎉</h1>
+            <p>새로운 기능을 테스트 중이에요.</p>
+        </div>
+    );
 }
 
 export default App;
